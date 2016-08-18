9e449c97cc275d6912e1d55cce8fdd96d05fcd31
Geoffrey Martin <geoffreymartin@hotmail.co.uk>
2016-08-18 15:27:58 +0100

    UCHV-62 - added the complainants step (#10)
    * ukvi-62 - added the complainants step
    * added the step field and section to the route config
    * added addition skeleton steps for the personal and contact detail to the route config
    * added rule to eslint in acceptance tests for generator function
    * add field mixins and validation to field config
    * added the field and validation translatioins
    * added the acceptance tests for the step
    
    * uchv-62 - modified acceptance test for complainant name

--- apps/complaints/index.js
+++ apps/complaints/index.js
@@ -12,26 +12,42 @@ module.exports = {
     '/who': {
       fields: ['applicant'],
       next: '/applicant-name',
-      forks: [{
-        target: '/declaration',
-        condition: {
-          field: 'applicant',
-          value: 'false'
-        }
-      }],
-      locals: {
-        section: 'personal-contact-details'
-      }
-    },
-    '/declaration': {
       locals: {
         section: 'personal-contact-details'
       }
     },
     '/applicant-name': {
+      fields: ['applicant-given-name', 'applicant-family-name'],
+      next: '/applicant-dob',
       locals: {
         section: 'personal-contact-details'
       }
     }
+  },
+  '/applicant-dob': {
+    next: '/applicant-contact',
+    locals: {
+      section: 'personal-contact-details'
+    }
+  },
+  '/applicant-contact': {
+    next: '/complaint-type',
+    locals: {
+      section: 'personal-contact-details'
+    }
+  },
+  '/representative-name': {
+    next: '/representative-contact',
+    locals: {
+      section: 'personal-contact-details'
+    }
+  },
+  '/representative-contact': {
+    next: '/complaint-type',
+    locals: {
+      section: 'personal-contact-details'
+    }
+  },
+  '/complaint-type': {
   }
 };

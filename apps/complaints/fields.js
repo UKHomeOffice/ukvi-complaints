a687634c34770ce621f58373a7c80d1dce3faa86
joefitter <hello@joefitter.com>
2016-09-02 15:16:16 +0100

    formatting

--- apps/complaints/fields.js
+++ apps/complaints/fields.js
@@ -2,25 +2,25 @@
 
 module.exports = {
 	applicant: {
-    mixin: 'radio-group',
-    validate: ['required'],
-    className: ['form-group'],
-    options: [{
-      value: 'true',
-      label: 'fields.applicant.options.true.label'
-    }, {
-      value: 'false',
-      label: 'fields.applicant.options.false.label',
+		mixin: 'radio-group',
+		validate: ['required'],
+		className: ['form-group'],
+		options: [{
+			value: 'true',
+			label: 'fields.applicant.options.true.label'
+		}, {
+			value: 'false',
+			label: 'fields.applicant.options.false.label',
 			toggle: 'accept-declaration',
 			child: `<div id="accept-declaration-panel" class="reveal js-hidden">
-    						<div class="panel-indent">
+								<div class="panel-indent">
 									<p>{{#t}}fields.accept-declaration.p1{{/t}}</p>
 									<p>{{#t}}fields.accept-declaration.p2{{/t}}</p>
-        					{{#checkbox}}accept-declaration{{/checkbox}}
-    						</div>
+									{{#checkbox}}accept-declaration{{/checkbox}}
+								</div>
 							</div>`
-    }]
-  },
+		}]
+	},
 	'accept-declaration': {
 		validate: 'required',
 		dependent: {
@@ -28,65 +28,65 @@ module.exports = {
 			value: 'false'
 		}
 	},
-  'applicant-name': {
-    mixin: 'input-text',
-    validate: 'required'
-  },
+	'applicant-name': {
+		mixin: 'input-text',
+		validate: 'required'
+	},
 	'representative-name': {
 		mixin: 'input-text',
 		validate: 'required'
 	},
-  'dob': {
-  },
-  'dob-day': {
-    validate: ['required', 'numeric']
-  },
-  'dob-month': {
-    validate: ['required', 'numeric']
-  },
-  'dob-year': {
-    validate: ['required', 'numeric']
-  },
-  'complaint-type': {
-    mixin: 'radio-group',
-    validate: ['required'],
-    className: ['form-group'],
-    options: [{
-      value: 'previous',
-      label: 'fields.complaint-type.options.previous'
-    }, {
-      value: 'staff',
-      label: 'fields.complaint-type.options.staff'
-    }, {
-      value: 'appointment',
-      label: 'fields.complaint-type.options.appointment'
-    }, {
-      value: 'refund',
-      label: 'fields.complaint-type.options.refund'
-    }, {
-      value: 'quality',
-      label: 'fields.complaint-type.options.quality'
-    }, {
-      value: 'something',
-      label: 'fields.complaint-type.options.something'
-    }]
-  },
-  'where': {
-    mixin: 'radio-group',
-    validate: ['required'],
-    className: ['form-group'],
-    options: [{
-      value: 'phone',
-      label: 'fields.where.options.phone'
-    }, {
-      value: 'visa-application-centre',
-      label: 'fields.where.options.visa-application-centre'
-    }, {
-      value: 'premium-service-centre',
-      label: 'fields.where.options.premium-service-centre'
-    }, {
-      value: 'letter',
-      label: 'fields.where.options.letter'
-    }]
-  }
+	'dob': {
+	},
+	'dob-day': {
+		validate: ['required', 'numeric']
+	},
+	'dob-month': {
+		validate: ['required', 'numeric']
+	},
+	'dob-year': {
+		validate: ['required', 'numeric']
+	},
+	'complaint-type': {
+		mixin: 'radio-group',
+		validate: ['required'],
+		className: ['form-group'],
+		options: [{
+			value: 'previous',
+			label: 'fields.complaint-type.options.previous'
+		}, {
+			value: 'staff',
+			label: 'fields.complaint-type.options.staff'
+		}, {
+			value: 'appointment',
+			label: 'fields.complaint-type.options.appointment'
+		}, {
+			value: 'refund',
+			label: 'fields.complaint-type.options.refund'
+		}, {
+			value: 'quality',
+			label: 'fields.complaint-type.options.quality'
+		}, {
+			value: 'something',
+			label: 'fields.complaint-type.options.something'
+		}]
+	},
+	'where': {
+		mixin: 'radio-group',
+		validate: ['required'],
+		className: ['form-group'],
+		options: [{
+			value: 'phone',
+			label: 'fields.where.options.phone'
+		}, {
+			value: 'visa-application-centre',
+			label: 'fields.where.options.visa-application-centre'
+		}, {
+			value: 'premium-service-centre',
+			label: 'fields.where.options.premium-service-centre'
+		}, {
+			value: 'letter',
+			label: 'fields.where.options.letter'
+		}]
+	}
 };

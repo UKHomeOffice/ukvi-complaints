7d632a61f95aea4c26e86304065c333131aec14d
Geoffrey Martin <geoffreymartin@Geoffreys-MacBook-Pro-2.local>
2016-09-21 11:21:33 +0100

    uchv-48 - move partial details summary from common to app * reverted app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const path = require('path');
 const bootstrap = require('hof-bootstrap');
 
 bootstrap({
 	translations: './apps/complaints/translations',
-  views: path.resolve(__dirname, './apps/common/views'),
+  views: false,
 	fields: false,
 	routes: [
 		require('./apps/complaints/')

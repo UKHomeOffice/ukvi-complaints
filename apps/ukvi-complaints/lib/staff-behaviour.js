43dd603b864a677ff73f1ad292b54fdaaf911cd0
Joseph Coker <JosephCoker@hod083420.home>
2021-03-11 15:19:22 +0000

    Finish formatting for StaffBehaviour

--- apps/ukvi-complaints/lib/staff-behaviour.js
+++ apps/ukvi-complaints/lib/staff-behaviour.js
@@ -12,29 +12,57 @@ class StaffBehaviourComplaint extends Complaint {
     switch (this.values['staff-behaviour']) {
       case 'face-to-face':
         return this.faceToFace();
+      case 'on-phone':
+        return this.onPhone();
+      case 'in-letter':
+        return this.inLetter();
     }
   }
 
   faceToFace() {
     const experience = {
       experienceType: 'FACE_TO_FACE',
-      location: {},
     };
 
     switch (this.values['which-centre']) {
       case 'vac':
-        experience.location.country = this.values['vac-country'];
-        experience.location.city = this.values['vac-city'];
-        break;
+        experience.location = {
+          country: this.values['vac-country'],
+          city: this.values['vac-city'],
+          centreType: 'VAC'
+        };
+        return experience;
       case 'ssc':
-        experience.location.city = this.values['ssc-city'];
-        break;
+        experience.location = {
+          city: this.values['ssc-city'],
+          centreType: 'SSC'
+        };
+        return experience;
       case 'ukvcas':
-        experience.location.city = this.values['ukvcas-city'];
-        break;
+        experience.location = {
+          city: this.values['ukvcas-city'],
+          centreType: 'UKVCAS'
+        };
+        return experience;
     }
+  }
 
-    return experience;
+  onPhone() {
+    return {
+      experienceType: 'PHONE',
+      callDetails: {
+        numberCalled: this.values['called-number'],
+        date: this.values['called-date'],
+        time: this.values['called-time'],
+        calledFrom: this.values['called-from']
+      }
+    };
+  }
+
+  inLetter() {
+    return {
+      experienceType: 'LETTER_OR_EMAIL',
+    };
   }
 }
 

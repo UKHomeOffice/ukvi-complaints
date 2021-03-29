97e0962b722c853e926f011ba3a88ec9fc2cd1fe
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2021-03-29 17:10:05 +0100

    Update node version and sql queue config * SD-272: add custom error page for form failing to submit to queue * SD-272: add max length constraint on all text fields * Add sqs queue config to config file * Increase test timeout * Remove integration test folder * Update node version and fix emailCasweworker config setting * update complaint details max length

--- apps/ukvi-complaints/behaviours/caseworker-email.js
+++ apps/ukvi-complaints/behaviours/caseworker-email.js
@@ -213,18 +213,15 @@ const getDataRows = (model, translate) => {
 };
 
 module.exports = config => {
-
-  if (!config.emailCaseworker) {
-    // Skips sending the email to caseworking inbox
-    return;
-  }
-
   if (config.transport !== 'stub' && !config.from && !config.replyTo) {
     // eslint-disable-next-line no-console
     console.warn('WARNING: Email `from` address must be provided. Falling back to stub email transport.');
   }
+
+  const transport = config.emailCaseworker ? config.transport : 'stub';
+
   return Emailer(Object.assign({}, config, {
-    transport: config.from ? config.transport : 'stub',
+    transport: config.from ? transport : 'stub',
     recipient: config.caseworker,
     subject: (model, translate) => translate('pages.email.caseworker.subject'),
     template: path.resolve(__dirname, '../emails/caseworker.html'),

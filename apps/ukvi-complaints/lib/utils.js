bec1e532d5031f2b5cef098c30a6ae5335ae1c32
Joseph Coker <JosephCoker@hod083420.home>
2021-04-15 15:08:42 +0100

    inject validator as argument to validAgainstSchema, add validatorFactory

--- apps/ukvi-complaints/lib/utils.js
+++ apps/ukvi-complaints/lib/utils.js
@@ -1,14 +1,12 @@
 'use strict';
 const { v4: uuidv4 } = require('uuid');
-const Validator = require('jsonschema').Validator;
 const { Producer } = require('sqs-producer');
 const config = require('../../../config');
 const decsSchema = require('../schema/decs.json');
 
-const validateAgainstSchema = (complaintData) => {
+const validAgainstSchema = (data, validator) => {
   try {
-    const validator = new Validator();
-    const valid = validator.validate(complaintData, decsSchema);
+    const valid = validator.validate(data, decsSchema);
 
     if (valid.errors.length > 0) {
       throw new Error(valid.errors);
@@ -23,7 +21,6 @@ const validateAgainstSchema = (complaintData) => {
 
 const sendToQueue = (complaintData) => {
   try {
-
     const producer = Producer.create(config.awsSqs);
 
     return new Promise((resolve, reject) => {
@@ -34,7 +31,8 @@ const sendToQueue = (complaintData) => {
             body: JSON.stringify(complaintData)
           }
         ])
-      .then(() => {
+      .then((response) => {
+        console.log(response);
         resolve();
       })
       .catch(error => {
@@ -47,6 +45,6 @@ const sendToQueue = (complaintData) => {
 };
 
 module.exports = {
-  validateAgainstSchema,
+  validAgainstSchema,
   sendToQueue,
 };
--- test/unit/lib/utils.spec.js
+++ test/unit/lib/utils.spec.js
@@ -8,6 +8,10 @@ describe('utils', () => {
   let createSub;
   let sendStub;
   let uuidStub;
+  let testSchema = {
+    'test': 'schema'
+  };
+
   const testUuid = '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d';
   const validComplaintData = {
     creationDate: '2021-03-17',
@@ -62,7 +66,8 @@ describe('utils', () => {
       },
       'uuid': {
         v4: uuidStub
-      }
+      },
+      '../schema/decs.json': testSchema
     });
   });
 
@@ -71,19 +76,37 @@ describe('utils', () => {
   });
 
   describe('#validateAgainstSchema', () => {
+    let testValidator;
+    let validateStub;
+
+    beforeEach(() => {
+      validateStub = sinon.stub();
+      validateStub.withArgs(validComplaintData, testSchema).returns({
+        errors: []
+      });
+      validateStub.withArgs(invalidComplaintData, testSchema).returns({
+        errors: [
+          'validation error'
+        ]
+      });
+
+      testValidator = {
+        validate: validateStub
+      };
+    });
 
     it('returns true if the complaintData object fits the schema', () => {
-      expect(utils.validateAgainstSchema(validComplaintData)).to.eql(true);
+      expect(utils.validAgainstSchema(validComplaintData, testValidator)).to.eql(true);
+    });
+
+    it('calls validate method on the validator once', () => {
+      utils.validAgainstSchema(validComplaintData, testValidator);
+
+      expect(validateStub).to.have.been.calledOnceWith(validComplaintData, testSchema);
     });
 
     it('throws an error if the complaintData object does not fit the schema', () => {
-      expect(() => utils.validateAgainstSchema(invalidComplaintData)).to.throw();
-    });
-
-    it('throws an error if the complaintData object contains "undefined" value', () => {
-      const invalidComplaintDetails = Object.assign({}, validComplaintData);
-      invalidComplaintDetails.complaint.complaintDetails.complaintText = undefined;
-      expect(() => utils.validateAgainstSchema(invalidComplaintDetails)).to.throw();
+      expect(() => utils.validAgainstSchema(invalidComplaintData, testSchema)).to.throw();
     });
 
   });

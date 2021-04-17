5248e8dae7a4c7d085c5fbf1cff9111a1d848be2
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2021-04-17 13:25:08 +0100

    rework index file to allow automation for integration testing

--- index.js
+++ index.js
@@ -2,14 +2,35 @@
 'use strict';
 const hof = require('hof');
 
-const settings = require('./hof.settings');
+const addDynamicSettings = (settings) => {
+  settings.routes = settings.routes.map(route => require(route));
+  settings.root = __dirname;
+  settings.start = false;
 
-if (process.env.REDIS_URL) {
-  settings.redis = process.env.REDIS_URL;
-}
+  // suppress logs when running the app in silent mode for automation testing, so app logs do not mix with test logs
+  if (process.argv.some(commandLineArgument => commandLineArgument === '\silent')) {
+    settings.loglevel = 'silent';
+  }
 
-settings.routes = settings.routes.map(route => require(route));
-settings.root = __dirname;
-settings.start = false;
+  if (process.env.REDIS_URL) {
+    settings.redis = process.env.REDIS_URL;
+  }
 
-module.exports = hof(settings);
+  return settings;
+};
+
+/*
+ During automation test setup, we ping the app with '?automation-test' query to check it is ready before running tests.
+ Put a cookie on this request so it doesn't fail HOF-middleware cookie check.
+*/
+const addAutomationTestCookie = (req, res, next) => {
+  if (req.query['automation-test'] !== undefined) {
+    req.cookies.testCookie = 'test';
+  }
+  return next();
+};
+
+const app = hof(addDynamicSettings(require('./hof.settings')));
+app.use('/reason', (req, res, next) => addAutomationTestCookie(req, res, next));
+
+module.exports = app;

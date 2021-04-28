e938c61aeedf81d3fae4e302b4e1ec92e504ed83
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2021-04-28 16:43:55 +0100

    add mock sqs setup files and integration tests

---
+++ test/integration/lib/get-from-sqs.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// reusable function which returns the messages on the SQS queue
+
+const AWS = require('aws-sdk');
+const SQS = AWS.SQS;
+
+async function get() {
+  const queue = new SQS({
+    endpoint: 'http://localhost:9324',
+    region: 'eu-west-2', // it does not matter
+    accessKeyId: 'example',
+    secretAccessKey: 'example'
+  });
+
+  return await queue.receiveMessage({QueueUrl: 'http://localhost:9324/queue/first-queue'}).promise();
+}
+
+module.exports.get = get;
---
+++ test/integration/test/making-an-apointment.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+const sqs = require('../lib/get-from-sqs');
+
+describe('\'Making an appointment\' complaint', () => {
+
+  it('should add the data to the SQS queue', async() => {
+    // Select correct complaint category
+    await page.goto(baseURL);
+    const complaintTypeRadio = await page.$('input#reason-immigration-appointment');
+    await complaintTypeRadio.check();
+    await submitPage();
+
+    // Problem radio
+    const problemRadio = await page.$('input#immigration-appointment-complain-appointments');
+    await problemRadio.check();
+    await submitPage();
+
+    // No reference number radio
+    const referenceNumberRadio = await page.$('input#reference-numbers-none');
+    await referenceNumberRadio.check();
+    await submitPage();
+
+    // Not filling out on behalf of someone else
+    const agentRadio = await page.$('input#acting-as-agent-no');
+    await agentRadio.check();
+    await submitPage();
+
+    // Name
+    await page.fill('#applicant-name', 'Name');
+    await submitPage();
+
+    // DOB
+    await page.fill('#applicant-dob-day', '1');
+    await page.fill('#applicant-dob-month', '1');
+    await page.fill('#applicant-dob-year', '2000');
+    await submitPage();
+
+    // Nationality
+    await page.fill('#applicant-nationality', 'United Kingdom');
+    await submitPage();
+
+    // Contact details
+    await page.fill('#applicant-email', 'test@test.com');
+    await submitPage();
+
+    // Complaint details
+    await page.fill('#complaint-details', 'test');
+    await submitPage();
+
+    // Check your answers page
+    await submitPage();
+
+    // retrieve queue and assert it is length 1
+    const queue = await sqs.get();
+    expect(queue.Messages.length).to.equal(1);
+
+  });
+
+});

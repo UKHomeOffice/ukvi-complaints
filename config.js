8b37563d13030de5e558f2bba32cf093616aef37
Joseph Coker <JosephCoker@hod083420.local>
2021-05-13 10:46:47 +0100

    add environment variables for toggling send to queue and email caseworker behaviour
    Change email caseworker to true to ensure it continus to work
    
    Remove vscode settings file

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   dateFormat: 'DD-MM-YYYY',
   dateTimeFormat: 'DD-MM-YYYY, hh:mma',
   redis: {
-    password: process.env.REDIS_PASSWORD
+    password: process.env.REDIS_PASSWORD,
   },
   email: {
     from: process.env.FROM_ADDRESS || 'stub-email@stub.com',
@@ -16,15 +16,17 @@ module.exports = {
     recipient: process.env.CASEWORKER_EMAIL || 'stub-email@stub.com',
     transportOptions: {
       accessKeyId: process.env.HOF_SES_USER || process.env.AWS_USER || '',
-      secretAccessKey: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD || ''
+      secretAccessKey:
+        process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD || '',
     },
     emailCaseworker: true,
   },
   awsSqs: {
     region: process.env.AWS_REGION || 'eu-west-2',
-    queueUrl: process.env.SQS_URL || 'http://localhost:4566/000000000000/local-queue',
+    queueUrl:
+      process.env.SQS_URL || 'http://localhost:4566/000000000000/local-queue',
     accessKeyId: process.env.ACCESS_KEY_ID || '',
     secretAccessKey: process.env.SECRET_ACCESS_KEY || '',
   },
-  sendToQueue: true,
+  sendToQueue: process.env.SEND_TO_DECS_QUEUE === 'true',
 };

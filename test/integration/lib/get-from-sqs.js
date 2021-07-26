a6c853b41ac5ecaa29c146c1fbc50c5ad03ae291
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2021-07-26 15:09:03 +0100

    Add code to test local SQS queue

---
+++ test/integration/lib/get-from-sqs.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// reusable function which returns the first message on the SQS queue
+
+const AWS = require('aws-sdk');
+const SQS = AWS.SQS;
+
+async function get() {
+  const queue = new SQS({
+    endpoint: 'http://localhost:9324',
+    region: 'eu-west-2',
+    accessKeyId: 'example',
+    secretAccessKey: 'example'
+  });
+  // return await queue.receiveMessage({QueueUrl: 'http://localhost:9324/queue/first-queue'}).promise();
+  const item = await queue.receiveMessage({QueueUrl: 'http://localhost:9324/queue/first-queue'}).promise();
+  console.log(item.Messages);
+}
+
+get();

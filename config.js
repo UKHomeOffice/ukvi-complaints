876583b629f70e0d640f32f5d5593f503662855e
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2022-06-27 18:52:03 +0100

    AWS Signature 4 authentication requires host(with correct region) and port which are passed in as options to nodemailer create transport

--- config.js
+++ config.js
@@ -9,17 +9,24 @@ module.exports = {
     password: process.env.REDIS_PASSWORD
   },
   email: {
-    from: 'emmanuel@ukvi-complaints.sas-notprod.homeoffice.gov.uk',
-    replyTo: 'emmanuel@ukvi-complaints.sas-notprod.homeoffice.gov.uk',
+    from: process.env.FROM_ADDRESS,
+    replyTo: process.env.REPLY_TO,
     transport: process.env.EMAIL_TRANSPORT || 'ses',
-    caseworker: process.env.CASEWORKER_EMAIL || 'stub@stub.com',
-    recipient: process.env.CASEWORKER_EMAIL || 'stub@stub.com',
+    caseworker: process.env.CASEWORKER_EMAIL || 'sas-hof-test@digital.homeoffice.gov.uk',
+    recipient: process.env.CASEWORKER_EMAIL || 'sas-hof-test@digital.homeoffice.gov.uk',
     transportOptions: {
       accessKeyId: process.env.HOF_SES_USER || process.env.AWS_USER || 'stub',
-      secretAccessKey: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD || 'stub'
+      secretAccessKey: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD || 'stub',
+      host: process.env.EMAIL_HOST || 'email-smtp.eu-west-1.amazonaws.com',
+      port: process.env.EMAIL_PORT || 587,
+      auth: {
+        user: process.env.HOF_SES_USER || process.env.AWS_USER ,
+        pass: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD
+      },
     },
     emailCaseworker: true
   },
+
   awsSqs: {
     region: process.env.AWS_REGION || 'eu-west-2',
     queueUrl: process.env.SQS_URL || 'http://localhost:9324/queue/first-queue',

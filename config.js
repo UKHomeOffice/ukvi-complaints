154d9ef19b46dae1ac96fce75a4130c2f7fb9f28
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2022-08-15 14:35:23 +0100

    Merge Conflicts

--- config.js
+++ config.js
@@ -9,21 +9,26 @@ module.exports = {
     password: process.env.REDIS_PASSWORD
   },
   email: {
-    from: process.env.FROM_ADDRESS || 'stub@stub.com',
-    replyTo: process.env.REPLY_TO || 'stub@stub.com',
-    transport: process.env.EMAIL_TRANSPORT || 'ses',
-    caseworker: process.env.CASEWORKER_EMAIL || 'stub@stub.com',
-    recipient: process.env.CASEWORKER_EMAIL || 'stub@stub.com',
+    from: process.env.FROM_ADDRESS,
+    replyTo: process.env.REPLY_TO,
+    transport: process.env.EMAIL_TRANSPORT || 'smtp',
+    caseworker: process.env.CASEWORKER_EMAIL || 'sas-hof-test@digital.homeoffice.gov.uk',
+    recipient: process.env.CASEWORKER_EMAIL || 'sas-hof-test@digital.homeoffice.gov.uk',
     transportOptions: {
       accessKeyId: process.env.HOF_SES_USER || process.env.AWS_USER || 'stub',
-      secretAccessKey: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD || 'stub'
+      secretAccessKey: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD || 'stub',
+      host: process.env.EMAIL_HOST || 'email-smtp.eu-west-1.amazonaws.com',
+      port: process.env.EMAIL_PORT || 465,
+      auth: {
+        user: process.env.HOF_SES_USER || process.env.AWS_USER,
+        pass: process.env.HOF_SES_PASSWORD || process.env.AWS_PASSWORD
+      }
     },
     emailCaseworker: true
   },
   hosts: {
     acceptanceTests: process.env.ACCEPTANCE_HOST_NAME || `http://localhost:${process.env.PORT || 8080}`
   },
-
   awsSqs: {
     region: process.env.AWS_REGION || 'eu-west-2',
     queueUrl: process.env.SQS_URL || 'http://localhost:9324/queue/first-queue',

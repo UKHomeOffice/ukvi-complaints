0640366657dac82f2867e27bacad208a2d1962c0
Chisom Oguibe <122998307+ChisomOguibe11@users.noreply.github.com>
2023-06-21 14:39:10 +0100

    UKVIC-167: add new CSP rules to service config (#240)
    * add new CSP rules to service config
    
    * lint fix - add missing semicolon
    
    * hotfix - indentations
    
    * update sonar scanner image to latest

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const settings = require('./hof.settings');
 
 settings.routes = settings.routes.map(route => require(route));
 settings.root = __dirname;
+settings.csp = {
+  imgSrc: [
+    'www.google-analytics.com',
+    'ssl.gstatic.com',
+    'www.google.co.uk/ads/ga-audiences'
+  ],
+  connectSrc: [
+    'https://www.google-analytics.com',
+    'https://region1.google-analytics.com',
+    'https://region1.analytics.google.com'
+  ]
+};
 
 if (process.env.REDIS_URL) {
   settings.redis = process.env.REDIS_URL;

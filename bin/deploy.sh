9ced4507104412466dfcdf404754260a64f669ee
adityababumallisettiHO <Aditya.Mallisetti@digital.homeoffice.gov.uk>
2025-02-17 16:25:49 +0000

    UKVIC-246: Add Filevault deployment
    * For the mew UKVIC amend form we are adding File vault
    * Add Filevault to deploy script to deploy and tear now as per requirements
    * Requested ACP to permit the ingress urls by changing the policy admissions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,7 +13,7 @@ if [[ $1 == 'tear_down' ]]; then
   export DRONE_SOURCE_BRANCH=$(cat /root/.dockersock/branch_name.txt)
 
   $kd --delete -f kube/configmaps/configmap.yml
-  $kd --delete -f kube/redis -f kube/app
+  $kd --delete -f kube/redis -f kube/app -f kube/file-vault
   echo "Torn Down UAT Branch - ukvic-$DRONE_SOURCE_BRANCH.internal.$BRANCH_ENV.homeoffice.gov.uk"
   exit 0
 fi
@@ -23,16 +23,16 @@ export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/configmaps -f kube/certs
-  $kd -f kube/redis -f kube/app
+  $kd -f kube/redis -f kube/file-vault -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml -f kube/app
+  $kd -f kube/configmaps/configmap.yml -f kube/file-vault -f kube/app
   $kd -f kube/redis
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/configmaps/configmap.yml -f kube/file-vault -f kube/app/service.yml
   $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
   $kd -f kube/redis -f kube/app/deployment.yml
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/configmaps/configmap.yml -f kube/file-vault -f kube/app/service.yml
   $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
   $kd -f kube/redis -f kube/app/deployment.yml
 fi

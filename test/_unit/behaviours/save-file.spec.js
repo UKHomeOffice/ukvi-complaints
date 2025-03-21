c27d79ce2ed811bf71ba4bfd15105813da5f97f7
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2025-03-21 09:59:45 +0000

    EPP-234 additional unit tests

--- test/_unit/behaviours/save-file.spec.js
+++ test/_unit/behaviours/save-file.spec.js
@@ -1,185 +1,267 @@
-'use strict';
+const proxyquire = require('proxyquire');
 
-const SaveDocumentBehaviour = require('../../../apps/ukvi-complaints/behaviours/save-file');
-
-describe('Test for Save Document Behaviour', () => {
-  class Base {
-    process() {}
-    locals() {}
-    saveValues() {}
-    validateField() {}
-  }
-
+describe('save-file behaviour tests', () => {
+  let SaveFile;
+  let mockConfig;
+  let mockModel;
   let req;
   let res;
   let next;
-
-  let instance;
-
-  const doc = {
-    document: {
-      name: 'test.pdf',
-      encoding: '7bit',
-      mimetype: 'pdf',
-      truncated: false,
-      size: 123456
-    }
-  };
+  let superclass;
 
   beforeEach(() => {
-    req = reqres.req();
-    res = reqres.res();
-    req.files = doc;
+    mockConfig = {
+      upload: {
+        maxFileSizeInBytes: 5000000,
+        allowedMimeTypes: ['image/jpeg', 'application/pdf'],
+        documentCategories: {
+          testDocument: {
+            allowMultipleUploads: true,
+            limit: 2,
+            limitValidationError: 'maxFileError'
+          }
+        }
+      }
+    };
+
+    mockModel = sinon.stub();
+    mockModel.prototype.save = sinon.stub().resolves();
+    mockModel.prototype.toJSON = sinon.stub().returns({ id: '12345' });
+    SaveFile = proxyquire(
+      '../../../apps/ukvi-complaints/behaviours/save-file',
+      {
+        '../../../config': mockConfig,
+        '../models/file-upload': mockModel
+      }
+    );
+
+    req = {
+      files: {},
+      form: { values: {} },
+      sessionModel: {
+        get: sinon.stub(),
+        set: sinon.stub()
+      },
+      log: sinon.stub()
+    };
+
+    res = {
+      redirect: sinon.stub()
+    };
+
+    next = sinon.stub();
+
+    superclass = class {
+      process() {}
+      validateField() {}
+      saveValues() {}
+    };
+
+    superclass.prototype.ValidationError = class ValidationError {
+      constructor(key, options) {
+        this.key = key;
+        this.type = options.type;
+        this.arguments = options.arguments;
+      }
+    };
+  });
+  it('should process the file and set the file', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = { name: 'test-file.pdf' };
+    sinon.stub(superclass.prototype, 'process');
+    instance.process(req);
+
+    expect(req.form.values.testField).to.equal('test-file.pdf');
+    expect(req.log.calledWith('info')).to.be.true;
+
+    superclass.prototype.process.restore();
   });
 
-  describe('Process file tests', () => {
-    before(() => {
-      sinon.stub(Base.prototype, 'process');
-      instance = new (SaveDocumentBehaviour(
-        'documentName',
-        'file-upload'
-      )(Base))();
-    });
+  it('should validate a valid file', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      size: 4000,
+      mimetype: 'image/jpeg',
+      truncated: false
+    };
 
-    it('should be called ', () => {
-      instance.process(req);
-      expect(Base.prototype.process).to.have.been.called;
-    });
+    req.sessionModel.get.returns([]);
+    sinon.stub(superclass.prototype, 'validateField');
+    const result = instance.validateField('testField', req);
+    expect(result).to.be.undefined;
 
-    it('Should attach the given file', () => {
-      const updatedDoc = {
-        'file-upload': {
-          name: 'test.pdf',
-          encoding: '7bit',
-          mimetype: 'pdf',
-          truncated: false,
-          size: 123456
-        }
-      };
-      req.files = updatedDoc;
-      instance.process(req);
-      expect(req.files).to.eql(updatedDoc);
-    });
-
-    after(() => {
-      Base.prototype.process.restore();
-    });
+    superclass.prototype.validateField.restore();
   });
 
-  describe('Save file locals tests', () => {
-    before(() => {
-      sinon.stub(Base.prototype, 'locals').returns(req, res, next);
-      instance = new (SaveDocumentBehaviour(
-        'documentName',
-        'file-upload'
-      )(Base))();
-    });
+  it('should return validation error - oversized file', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      size: 6000000,
+      mimetype: 'image/jpeg',
+      truncated: false
+    };
 
-    it('init - locals', () => {
-      req.form.errors = {};
-      instance.locals(req, res, next);
-      expect(Base.prototype.locals).to.have.been.called;
-    });
+    sinon.stub(superclass.prototype, 'validateField');
+
+    const error = instance.validateField('testField', req);
+    expect(error).to.be.instanceOf(superclass.prototype.ValidationError);
+    expect(error.type).to.equal('maxFileSize');
+
+    superclass.prototype.validateField.restore();
   });
 
-  describe('Save values tests', () => {
-    before(() => {
-      sinon.stub(Base.prototype, 'saveValues').returns(req, res, next);
-      instance = new (SaveDocumentBehaviour(
-        'documentName',
-        'file-upload'
-      )(Base))();
-    });
+  it('should return validation error - invalid mimetype', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      size: 2000,
+      mimetype: 'text/plain',
+      truncated: false
+    };
 
-    it('init - saveValues', () => {
-      const updatedDoc = {
-        'file-upload': {
-          name: 'test.pdf',
-          encoding: '7bit',
-          mimetype: 'pdf',
-          truncated: false,
-          size: 123456
-        }
-      };
-      req.files = updatedDoc;
-      instance.saveValues(req, res, next);
-      expect(Base.prototype.saveValues.called).to.be.false;
-    });
+    sinon.stub(superclass.prototype, 'validateField');
 
-    it('Should save the file in the session model', () => {
-      req.sessionModel.set('documentName', doc);
-      instance.saveValues(req, res, next);
-      const sessionModel = req.sessionModel.get('documentName');
-      expect(sessionModel.document.name).to.eql('test.pdf');
-    });
+    const error = instance.validateField('testField', req);
+    expect(error).to.be.instanceOf(superclass.prototype.ValidationError);
+    expect(error.type).to.equal('fileType');
 
-    it('should redirect to current route after upload', () => {
-      req.form.options.route = '/test-route';
-      req.sessionModel.set('documentName', doc);
-      instance.saveValues(req, res, next);
-      expect(req.form.options.route).to.eql('/test-route');
-    });
-
-    after(() => {
-      Base.prototype.saveValues.restore();
-    });
+    superclass.prototype.validateField.restore();
   });
 
-  describe('Validate field tests', () => {
-    before(() => {
-      sinon.stub(Base.prototype, 'validateField').returns('file-upload', req);
-      instance = new (SaveDocumentBehaviour(
-        'upload-complaint-doc',
-        'file-upload'
-      )(Base))();
-    });
+  it('should return validation error - exceeding upload limit', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      size: 2000,
+      mimetype: 'image/jpeg',
+      truncated: false
+    };
 
-    it('validateField - valid', () => {
-      Base.prototype.ValidationError = sinon.stub();
-      const updatedDoc = {
-        'file-upload': {
-          name: 'test.pdf',
-          encoding: '7bit',
-          mimetype: 'pdf',
-          truncated: false,
-          size: 123456
-        }
-      };
-      req.files = updatedDoc;
-      instance.validateField('file-upload', req);
-      expect(Base.prototype.validateField.called).to.be.false;
-    });
+    req.sessionModel.get.returns([{ id: '1' }, { id: '2' }]);
 
-    it('validateField - invalid size', () => {
-      Base.prototype.ValidationError = sinon.stub();
-      const updatedDoc = {
-        'file-upload': {
-          name: 'test.pdf',
-          encoding: '7bit',
-          mimetype: 'pdf',
-          truncated: true,
-          size: 2352652
-        }
-      };
-      req.files = updatedDoc;
-      instance.validateField('file-upload', req);
-      expect(Base.prototype.validateField.called).to.be.false;
-    });
+    sinon.stub(superclass.prototype, 'validateField');
 
-    it('validateField - invalid mimetype', () => {
-      Base.prototype.ValidationError = sinon.stub();
-      const updatedDoc = {
-        'file-upload': {
-          name: 'test.ppt',
-          encoding: '7bit',
-          mimetype: 'ppt',
-          truncated: false,
-          size: 12358
-        }
-      };
-      req.files = updatedDoc;
-      instance.validateField('file-upload', req);
-      expect(Base.prototype.validateField.called).to.be.false;
-    });
+    const error = instance.validateField('testField', req);
+    expect(error).to.be.instanceOf(superclass.prototype.ValidationError);
+    expect(error.type).to.equal('maxFileError');
+
+    superclass.prototype.validateField.restore();
+  });
+
+  it('should return validation error - truncated file', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      size: 2000,
+      mimetype: 'image/jpeg',
+      truncated: true
+    };
+
+    // check
+    sinon.stub(superclass.prototype, 'validateField');
+
+    const error = instance.validateField('testField', req);
+    expect(error).to.be.instanceOf(superclass.prototype.ValidationError);
+    expect(error.type).to.equal('maxFileSize');
+
+    superclass.prototype.validateField.restore();
+  });
+
+  it('should handle multiple uploads not allowed', () => {
+    mockConfig.upload.documentCategories.testDocument.allowMultipleUploads = false;
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      size: 2000,
+      mimetype: 'image/jpeg',
+      truncated: false
+    };
+
+    req.sessionModel.get.returns([{ id: '1' }]);
+
+    sinon.stub(superclass.prototype, 'validateField');
+
+    const error = instance.validateField('testField', req);
+    expect(error).to.be.undefined;
+
+    superclass.prototype.validateField.restore();
+  });
+
+  it('should successfully save the file and redirect', async () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      name: 'test-file.pdf',
+      data: Buffer.from('uploaded file data'),
+      mimetype: 'application/pdf'
+    };
+
+    req.sessionModel.get.returns([]);
+    sinon.stub(superclass.prototype, 'saveValues');
+    await instance.saveValues(req, res, next);
+    expect(mockModel.prototype.save.calledOnce).to.be.true;
+    expect(req.sessionModel.set.calledWith('testDocument')).to.be.true;
+    expect(res.redirect.calledOnce).to.be.true;
+
+    superclass.prototype.saveValues.restore();
+  });
+
+  it('should handle error during save - call next error', async () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    req.files.testField = {
+      name: 'test-file.pdf',
+      data: Buffer.from('uploaded file data'),
+      mimetype: 'application/pdf'
+    };
+
+    req.sessionModel.get.returns([]);
+    mockModel.prototype.save.rejects(new Error('save api error'));
+    sinon.stub(superclass.prototype, 'saveValues');
+    await instance.saveValues(req, res, next);
+    expect(next.calledOnce).to.be.true;
+    expect(next.args[0][0].message).to.equal(
+      'Failed to save document: Error: save api error'
+    );
+
+    superclass.prototype.saveValues.restore();
+  });
+
+  it('should not set the form when no file uploaded', () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    sinon.stub(superclass.prototype, 'process');
+    instance.process(req);
+
+    expect(req.form.values.testField).to.be.undefined;
+    superclass.prototype.process.restore();
+  });
+
+  it('should not save a file when no file uploaded', async () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    sinon.stub(superclass.prototype, 'saveValues');
+    await instance.saveValues(req, res, next);
+
+    expect(mockModel.prototype.save.called).to.be.false;
+    expect(req.sessionModel.set.called).to.be.false;
+    expect(res.redirect.called).to.be.false;
+
+    superclass.prototype.saveValues.restore();
+  });
+
+  it('should call and return super validateField', async () => {
+    const Bhaviour = SaveFile('testDocument', 'testField')(superclass);
+    const instance = new Bhaviour();
+    sinon
+      .stub(superclass.prototype, 'validateField')
+      .returns('super-validation-result');
+    const result = instance.validateField('testField', req);
+
+    expect(result).to.equal('super-validation-result');
+    superclass.prototype.validateField.restore();
   });
 });
--- test/_unit/models/file-upload.spec.js
+++ test/_unit/models/file-upload.spec.js
@@ -1,206 +1,140 @@
 const proxyquire = require('proxyquire');
 
-describe('UploadModel', () => {
+describe('File upload model tests', () => {
   let UploadModel;
-  let model;
-  let requestStub;
-  let loggerStub;
-  let uuidStub;
-  let formDataStub;
-  let formDataInstanceStub;
-  let configMock;
+  let mockConfig;
+  let mockLogger;
+  let mockRequest;
 
   beforeEach(() => {
-    requestStub = sinon.stub();
-    loggerStub = {
+    mockConfig = {
+      upload: {
+        hostname: 'htttp://file-valut'
+      },
+      keycloak: {
+        token: 'http://keycloak/token',
+        username: 'test-user',
+        password: 'test-pwd',
+        clientId: 'test-client-id',
+        secret: 'test-secret'
+      }
+    };
+
+    mockLogger = {
       error: sinon.stub(),
       info: sinon.stub()
     };
-    uuidStub = sinon.stub().returns('test-uuid');
-    formDataInstanceStub = {
-      append: sinon.stub(),
-      getHeaders: sinon.stub().returns({})
-    };
-    formDataStub = sinon.stub().returns(formDataInstanceStub);
+    mockRequest = sinon.stub();
 
-    configMock = {
-      upload: {
-        hostname: 'http://example.com'
-      },
-      keycloak: {
-        token: 'http://example.com/token',
-        username: 'user',
-        password: 'pass',
-        clientId: 'client',
-        secret: 'secret'
-      },
-      env: 'test'
-    };
     UploadModel = proxyquire(
       '../../../apps/ukvi-complaints/models/file-upload',
       {
-        hof: {
-          model: class {
-            constructor() {
-              this.attributes = {};
-            }
-            set(key, value) {
-              this.attributes[key] = value;
-            }
-            get(key) {
-              return this.attributes[key];
-            }
-            unset(key) {
-              delete this.attributes[key];
-            }
-            request() {}
-            url(attrs) {
-              return `${attrs.url}/upload`;
-            }
-          }
-        },
-        uuid: { v4: uuidStub },
-        'form-data': formDataStub,
-        'hof/lib/logger': () => loggerStub,
-        url: { parse: sinon.stub().returns({}) },
-        '../../../config': configMock
+        '../../../config': mockConfig,
+        'hof/lib/logger': () => mockLogger
       }
     );
-
-    model = new UploadModel();
   });
 
-  it('should set an id on instantiation', () => {
-    expect(model.get('id')).to.equal('test-uuid');
-  });
-
-  describe('#save', () => {
-    it('should throw an error if hostname is not defined', () => {
-      configMock.upload.hostname = null;
+  describe('save', () => {
+    it('should throw error - hostname not defined', () => {
+      delete mockConfig.upload.hostname;
+      const model = new UploadModel();
       expect(() => model.save()).to.throw('File-vault hostname is not defined');
-      expect(loggerStub.error.calledWith('File-vault hostname is not defined'))
+      expect(mockLogger.error.calledWith('File-vault hostname is not defined'))
         .to.be.true;
     });
 
-    it('should send a request with form data', async () => {
-      model.get = sinon
-        .stub()
-        .withArgs('data')
-        .returns('file-data')
-        .withArgs('name')
-        .returns('file-name')
-        .withArgs('mimetype')
-        .returns('file-mimetype');
-      model.request = requestStub.yields(null, {
-        url: 'http://example.com/file/test'
-      });
+    it('should reject when request fails', async () => {
+      const model = new UploadModel();
+      model.get = sinon.stub().returns('test-result');
+      mockRequest.yields(new Error('request failed'));
+
+      model.request = mockRequest;
+
+      try {
+        await model.save();
+      } catch (err) {
+        expect(err.message).to.deep.equal('File upload failed: request failed');
+      }
+    });
+
+    it('should throw error if no url in the response', async () => {
+      const model = new UploadModel();
+      model.get = sinon.stub().returns('test-result');
+      mockRequest.yields(null, {});
+
+      model.request = mockRequest;
+
+      try {
+        await model.save();
+      } catch (err) {
+        expect(mockLogger.error.called).to.be.true;
+      }
+    });
+
+    it('should unset data after successful request', async () => {
+      const model = new UploadModel();
+      model.get = sinon.stub().returns('test-result');
+      model.set = sinon.stub();
+      model.unset = sinon.stub();
+      mockRequest.yields(null, { url: 'http://file-vault/file/test' });
+
+      model.request = mockRequest;
 
       await model.save();
-
       expect(
-        formDataInstanceStub.append.calledWith('document', 'file-data', {
-          filename: 'file-name',
-          contentType: 'file-mimetype'
+        model.set.calledWith({
+          url: 'http://file-vault/file/generate-link/test'
         })
-      ).to.be.false;
-    });
-
-    it('should handle request errors', async () => {
-      model.get = sinon
-        .stub()
-        .withArgs('data')
-        .returns('file-data')
-        .withArgs('name')
-        .returns('file-name')
-        .withArgs('mimetype')
-        .returns('file-mimetype');
-      model.request = requestStub.yields(new Error('Test Error'), null);
-
-      try {
-        await model.save();
-      } catch (err) {
-        expect(err.message).to.equal('File upload failed: Test Error');
-        expect(
-          loggerStub.error.calledWith(
-            sinon.match.string.and(sinon.match(/Test Error/))
-          )
-        ).to.be.true;
-      }
-    });
-
-    it('should handle empty or invalid response', async () => {
-      model.get = sinon
-        .stub()
-        .withArgs('data')
-        .returns('file-data')
-        .withArgs('name')
-        .returns('file-name')
-        .withArgs('mimetype')
-        .returns('file-mimetype');
-      model.request = requestStub.yields(null, null);
-
-      try {
-        await model.save();
-      } catch (err) {
-        expect(err.message).to.equal(
-          'Received empty or invalid response from file-vault'
-        );
-        expect(
-          loggerStub.error.calledWith(
-            'Received empty or invalid response from file-vault'
-          )
-        ).to.be.true;
-      }
+      ).to.be.true;
     });
   });
 
-  describe('#auth', () => {
-    it('should throw an error if any keycloak property is not defined', async () => {
-      configMock.keycloak = {};
+  describe('auth', () => {
+    it('should throw error - required keycloak property is missing', async () => {
+      delete mockConfig.keycloak.username;
+
+      const model = new UploadModel();
       try {
         await model.auth();
       } catch (err) {
-        expect(err.message).to.include('Keycloak');
-        expect(
-          loggerStub.error.calledWith(
-            sinon.match.string.and(sinon.match(/Keycloak/))
-          )
-        ).to.be.true;
+        expect(err.message).to.equal('Keycloak username is not defined');
       }
     });
 
-    it('should retrieve access token successfully', async () => {
-      model._request = sinon
-        .stub()
-        .resolves({ data: { access_token: 'test-token' } });
+    it('should throw error when request fails', async () => {
+      const model = new UploadModel();
+      mockRequest.rejects({
+        response: {}
+      });
+      model._request = mockRequest;
+
+      try {
+        await model.auth();
+      } catch (err) {
+        expect(err).to.exist;
+      }
+    });
+
+    it('should throw error when response does not contain access token', async () => {
+      const model = new UploadModel();
+      mockRequest.resolves({ data: {} });
+      model._request = mockRequest;
+
+      try {
+        await model.auth();
+      } catch (err) {
+        expect(err).to.exist;
+      }
+    });
+
+    it('should return the token when auth is successful', async () => {
+      const model = new UploadModel();
+      mockRequest.resolves({ data: { access_token: 'test-token' } });
+      model._request = mockRequest;
 
       const result = await model.auth();
-
-      expect(result.bearer).to.equal('test-token');
-      expect(loggerStub.info.calledWith('Successfully retrieved access token'))
-        .to.be.true;
-    });
-
-    it('should handle errors while retrieving access token', async () => {
-      model._request = sinon.stub().rejects({
-        message: 'Request Error',
-        response: {
-          status: 500,
-          statusText: 'Internal Server Error',
-          data: {}
-        }
-      });
-
-      try {
-        await model.auth();
-      } catch (err) {
-        expect(err.message).to.include('Request Error');
-        expect(
-          loggerStub.error.calledWith(
-            sinon.match.string.and(sinon.match(/Request Error/))
-          )
-        ).to.be.true;
-      }
+      expect(result).to.deep.equal({ bearer: 'test-token' });
     });
   });
 });

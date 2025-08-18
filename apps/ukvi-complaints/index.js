cebb704ba36b21f2aa0d5f6aca508a20bc7a3584
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2025-08-18 10:28:05 +0100

    UKVIC-278 Revert "UKVIC-269-add second caseworker email (#299)"
    This reverts commit a45399d8d955243f9fa7e8abd85a4a4585199f9d.

--- apps/ukvi-complaints/index.js
+++ apps/ukvi-complaints/index.js
@@ -3,8 +3,7 @@
 const config = require('../../config');
 const conditionalContent = require('./behaviours/conditional-content');
 const customerEmailer = require('./behaviours/customer-email')(config.email);
-const caseworkerEmailerOne = require('./behaviours/caseworker-email')(config.email);
-const caseworkerEmailerTwo = require('./behaviours/caseworker-email-2')(config.email);
+const caseworkerEmailer = require('./behaviours/caseworker-email')(config.email);
 const SaveDocument = require('./behaviours/save-file');
 const RemoveDocument = require('./behaviours/remove-file');
 const sendToSQS = require('./behaviours/send-to-sqs');
@@ -887,14 +886,7 @@ module.exports = {
       next: '/confirm'
     },
     '/confirm': {
-      behaviours: [
-        sendToSQS,
-        caseworkerEmailerOne,
-        caseworkerEmailerTwo,
-        customerEmailer,
-        'complete',
-        require('hof').components.summary
-      ],
+      behaviours: [sendToSQS, caseworkerEmailer, customerEmailer, 'complete', require('hof').components.summary],
       next: '/complete',
       sections: {
         'complaint-details': [

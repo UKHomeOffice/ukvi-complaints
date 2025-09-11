e47d8d5ae8ef7a36ef3af144b8bd77fa0022edc0
anjurajanHO <206862292+anjurajanHO@users.noreply.github.com>
2025-09-11 13:49:32 +0100

    Fixed linting issue

--- test/_unit/behaviours/set-submission-reference.spec.js
+++ test/_unit/behaviours/set-submission-reference.spec.js
@@ -28,7 +28,8 @@ describe('set-submission-reference', () => {
     res = {};
     next = sinon.stub();
 
-    getValuesStub = sinon.stub(Base.prototype, 'getValues').callsFake((reqArg, resArg, nextArg) => reqArg);
+    // getValuesStub = sinon.stub(Base.prototype, 'getValues').callsFake((reqArg, resArg, nextArg) => reqArg);
+    getValuesStub = sinon.stub(Base.prototype, 'getValues').callsFake(reqArg => reqArg);
 
     const CustomClass = SetSubmissionReferenceBehaviour(Base);
     instance = new CustomClass();
@@ -47,7 +48,7 @@ describe('set-submission-reference', () => {
 
       expect(req.sessionModel.get.calledWith('submission-reference')).to.be.true;
       expect(req.sessionModel.set.calledWith('submission-reference', existingUuid)).to.be.true;
-      expect(req.log.calledWithMatch('info', sinon.match((msg) => msg.includes(existingUuid)))).to.be.true;
+      expect(req.log.calledWithMatch('info', sinon.match(msg => msg.includes(existingUuid)))).to.be.true;
     });
 
     it('should generate and set a new submission reference if not in session', () => {
@@ -56,7 +57,7 @@ describe('set-submission-reference', () => {
       instance.getValues(req, res, next);
 
       expect(req.sessionModel.set.calledWithMatch('submission-reference', sinon.match.string)).to.be.true;
-      expect(req.log.calledWithMatch('info', sinon.match((msg) => msg.includes('Submission Reference:')))).to.be.true;
+      expect(req.log.calledWithMatch('info', sinon.match(msg => msg.includes('Submission Reference:')))).to.be.true;
     });
   });
 });

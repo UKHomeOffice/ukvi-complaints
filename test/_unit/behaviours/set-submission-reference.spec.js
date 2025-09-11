7816487a00ba73326fb54420c7ea3eb3ec8b95d6
anjurajanHO <206862292+anjurajanHO@users.noreply.github.com>
2025-09-11 13:23:21 +0100

    UKVIC-287: Added unit test for save form details.

---
+++ test/_unit/behaviours/set-submission-reference.spec.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { v4: uuidv4 } = require('uuid');
+
+const SetSubmissionReferenceBehaviour = require('../../../apps/ukvi-complaints/behaviours/set-submission-reference');
+
+describe('set-submission-reference', () => {
+  class Base {
+    getValues() {}
+  }
+
+  let req;
+  let res;
+  let next;
+  let instance;
+  let getValuesStub;
+
+  beforeEach(() => {
+    req = {
+      sessionModel: {
+        set: sinon.stub(),
+        get: sinon.stub()
+      },
+      log: sinon.stub()
+    };
+    res = {};
+    next = sinon.stub();
+
+    getValuesStub = sinon.stub(Base.prototype, 'getValues').callsFake((reqArg, resArg, nextArg) => reqArg);
+
+    const CustomClass = SetSubmissionReferenceBehaviour(Base);
+    instance = new CustomClass();
+  });
+
+  afterEach(() => {
+    getValuesStub.restore();
+  });
+
+  describe('getValues', () => {
+    it('should set the submission reference if it exists in session', () => {
+      const existingUuid = uuidv4();
+      req.sessionModel.get.withArgs('submission-reference').returns(existingUuid);
+
+      instance.getValues(req, res, next);
+
+      expect(req.sessionModel.get.calledWith('submission-reference')).to.be.true;
+      expect(req.sessionModel.set.calledWith('submission-reference', existingUuid)).to.be.true;
+      expect(req.log.calledWithMatch('info', sinon.match((msg) => msg.includes(existingUuid)))).to.be.true;
+    });
+
+    it('should generate and set a new submission reference if not in session', () => {
+      req.sessionModel.get.withArgs('submission-reference').returns(undefined);
+
+      instance.getValues(req, res, next);
+
+      expect(req.sessionModel.set.calledWithMatch('submission-reference', sinon.match.string)).to.be.true;
+      expect(req.log.calledWithMatch('info', sinon.match((msg) => msg.includes('Submission Reference:')))).to.be.true;
+    });
+  });
+});

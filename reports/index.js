5abf4cb8d56b7b6ee2c08f14ba4a3b1d41b814db
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2025-09-16 09:19:48 +0100

    UKVIC-271 change timing to weekly and update reports

--- reports/index.js
+++ reports/index.js
@@ -3,8 +3,9 @@ const WeeklySubmittedReports = require('./weekly_submitted_reports');
 module.exports = class ReportsFactory {
   static async createReport(type, logger) {
     switch(type) {
-      case '24-hour-report':
+      case '1-week-report':
         return await WeeklySubmittedReports.createReport(type, logger);
     }
+    return null;
   }
 };

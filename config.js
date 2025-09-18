60896281b52257ac6ca67045fd7720e0795dc129
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2025-09-18 13:06:23 +0100

    UKVIC-271 fix linting

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
     caseworkerEmail: process.env.CASEWORKER_EMAIL,
     // noEmail sets default inbox where an email is expected by govNotify but not requested in the form
     noEmail: process.env.NO_EMAIL_GIVEN_INBOX,
-    csvReportTemplateId: process.env.CSV_REPORT_TEMPLATE_ID,
+    csvReportTemplateId: process.env.CSV_REPORT_TEMPLATE_ID
   },
   hosts: {
     acceptanceTests: process.env.ACCEPTANCE_HOST_NAME

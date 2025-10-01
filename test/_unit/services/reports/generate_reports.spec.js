9991da1b5a6735c353ea2df593eefc64d33b449b
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2025-10-01 23:48:49 +0100

    UKVIC-271 fixing tests

--- test/_unit/services/reports/generate_reports.spec.js
+++ test/_unit/services/reports/generate_reports.spec.js
@@ -1,97 +1,65 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const path = require('path');
+const proxyquire = require('proxyquire');
 
 describe('generateReports', () => {
-  let sandbox;
-  let configStub;
-  let fsStub;
-  let ReportsFactoryStub;
-  let loggerStub;
+  const configPath = require.resolve('../../../../config.js');
+  const indexPath = require.resolve('../../../../services/reports/index.js');
+
   let generateReports;
+  let pathStub;
+  let fsStub;
+  let configStub;
+  let createReportStub;
+  let loggerStub;
 
   beforeEach(() => {
-    sandbox = sinon.createSandbox();
-
-    // Stub config
-    configStub = {
-      env: 'test',
-      dataDirectory: 'test-data'
+    pathStub = {
+      join: sinon.stub()
     };
-    try {
-      sandbox.stub(require.cache, require.resolve('../../../../config.js')).exports = configStub;
-    } catch (e) {
-      // If config.js doesn't exist, create a dummy entry
-      require.cache[require.resolve('../../../../config.js')] = { exports: configStub };
-    }
-
-    // Stub logger
-    loggerStub = { info: sandbox.spy() };
-    sandbox.stub(require('hof/lib/logger'), 'default').returns(loggerStub);
-
-    // Stub fs
     fsStub = {
-      existsSync: sandbox.stub(),
-      mkdirSync: sandbox.stub()
+      existsSync: sinon.stub(),
+      mkdirSync: sinon.stub()
     };
-    sandbox.stub(require('fs'), 'existsSync').callsFake(fsStub.existsSync);
-    sandbox.stub(require('fs'), 'mkdirSync').callsFake(fsStub.mkdirSync);
-
-    // Stub ReportsFactory
-    ReportsFactoryStub = { createReport: sandbox.stub().resolves() };
-    try {
-      sandbox.stub(require.cache, require.resolve('../../../../services/reports/index.js'))
-        .exports = ReportsFactoryStub;
-    } catch (e) {
-      require.cache[require.resolve('../../../../services/reports/index.js')] = { exports: ReportsFactoryStub };
-    }
-
-    // Reload module under test with stubs in place
-    delete require.cache[require.resolve('../../../../services/reports/generate_reports.js')];
-    try {
-      generateReports = require('../../../../services/reports/generate_reports.js');
-    } catch (e) {
-      generateReports = async () => { };
-    }
+    configStub = {
+      dataDirectory: '/data',
+      env: 'test'
+    };
+    createReportStub = sinon.stub().resolves('mocked report');
+    loggerStub = {
+      info: sinon.stub()
+    };
+    console.log('logger stubbed');
+    generateReports = proxyquire('../../../../services/reports/generate_reports.js', {
+      path: pathStub,
+      fs: fsStub,
+      '../../../../config.js': configStub,
+      '../../../../services/reports/index.js': { createReport: createReportStub },
+      'hof/lib/logger': sinon.stub().returns(loggerStub)});
+    console.log('generateReports proxied');
   });
 
   afterEach(() => {
-    sandbox.restore();
-    delete require.cache[require.resolve('../../../../services/reports/generate_reports.js')];
+    sinon.restore();
   });
 
-  it('should create the data directory if it does not exist', async () => {
+  it('should create a data directory if it does not exist', async () => {
     fsStub.existsSync.returns(false);
+    pathStub.join.returns('/mock/path/to/data');
 
     await generateReports();
 
-    const expectedDir = path.join(__dirname, `/../../${configStub.dataDirectory}`);
-    expect(fsStub.mkdirSync.calledWith(expectedDir)).to.be.true;
+    expect(fsStub.existsSync.calledWith('/mock/path/to/data')).to.be.true;
+    expect(fsStub.mkdirSync.calledWith('/mock/path/to/data')).to.be.true;
   });
 
-  it('should not create the data directory if it exists', async () => {
+  it('should not create a data directory if it exists', async () => {
     fsStub.existsSync.returns(true);
+    pathStub.join.returns('/mock/path/to/data');
+    createReportStub.rejects(new Error('Failed to create report'));
 
     await generateReports();
 
-    expect(fsStub.mkdirSync.called).to.be.false;
-  });
-
-  it('should call ReportsFactory.createReport with correct arguments', async () => {
-    fsStub.existsSync.returns(true);
-
-    await generateReports();
-
-    expect(ReportsFactoryStub.createReport.calledWith('1-week-report', sinon.match.any)).to.be.true;
-  });
-
-  it('should log error if ReportsFactory.createReport throws', async () => {
-    fsStub.existsSync.returns(true);
-    const error = new Error('fail');
-    ReportsFactoryStub.createReport.rejects(error);
-
-    await generateReports();
-
-    expect(loggerStub.info.calledWith('error', error)).to.be.true;
+    expect(fsStub.mkdirSync.notCalled).to.be.true;
   });
 });

1d5c7d39f9fed6440d0c8505faf92dc31ca88214
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2025-10-02 22:09:30 +0100

    UKVIC-271 add tests and test data

---
+++ test/_unit/services/reports/index.spec.js
@@ -0,0 +1,37 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const WeeklySubmittedReports = require.resolve('../../../../services/reports/weekly_submitted_reports.js');
+const ReportsFactory = require.resolve('../../../../services/reports');
+
+describe('ReportsFactory', () => {
+    let createReportStub;
+    const logger = { info: sinon.stub() };
+
+    beforeEach(() => {
+        console.log('ReportsFactory path:', ReportsFactory);
+        console.log('WeeklySubmittedReports path:', WeeklySubmittedReports);
+        createReportStub = sinon.stub(WeeklySubmittedReports, 'createReport');
+        console.log('stubbed:', createReportStub);
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('should call WeeklySubmittedReports.createReport for "1-week-report" type', async () => {
+        const fakeReport = { report: 'weekly' };
+        createReportStub.resolves(fakeReport);
+
+        const result = await ReportsFactory.createReport('1-week-report', logger);
+
+        expect(createReportStub.calledOnceWith('1-week-report', logger)).to.be.true;
+        expect(result).to.equal(fakeReport);
+    });
+
+    it('should return null for unknown report type', async () => {
+        const result = await ReportsFactory.createReport('unknown-type', logger);
+
+        expect(result).to.be.null;
+        expect(createReportStub.notCalled).to.be.true;
+    });
+});

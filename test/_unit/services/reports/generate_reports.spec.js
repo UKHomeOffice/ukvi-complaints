cded6337c08e2f5611ed8c82894cf155fdd90336
anjurajanHO <206862292+anjurajanHO@users.noreply.github.com>
2025-10-13 17:54:44 +0100

    Unit test fixes

--- test/_unit/services/reports/generate_reports.spec.js
+++ test/_unit/services/reports/generate_reports.spec.js
@@ -3,9 +3,6 @@ const { expect } = require('chai');
 const proxyquire = require('proxyquire');
 
 describe('generateReports', () => {
-  // const configPath = require.resolve('../../../../config.js');
-  // const indexPath = require.resolve('../../../../services/reports/index.js');
-
   let generateReports;
   let pathStub;
   let fsStub;
@@ -13,30 +10,44 @@ describe('generateReports', () => {
   let createReportStub;
   let loggerStub;
 
+  const mockDataPath = '/mock/path/to/data';
+
   beforeEach(() => {
+    // Stub path.join to always return the mock path
     pathStub = {
-      join: sinon.stub()
+      join: sinon.stub().returns(mockDataPath)
     };
+
+    // Stub fs methods
     fsStub = {
       existsSync: sinon.stub(),
       mkdirSync: sinon.stub()
     };
+
+    // Stub config
     configStub = {
-      dataDirectory: '/data',
+      dataDirectory: 'data',
       env: 'test'
     };
+
+    // Stub createReport
     createReportStub = sinon.stub().resolves('mocked report');
+
+    // Stub logger
     loggerStub = {
       info: sinon.stub()
     };
+
+    // Load the module with proxyquire
     generateReports = proxyquire('../../../../services/reports/generate_reports.js', {
       path: pathStub,
       fs: fsStub,
-      '/../../config.js': configStub,
+      '../../config.js': configStub,
       './index.js': {
         createReport: createReportStub
       },
-      'hof/lib/logger': sinon.stub().returns(loggerStub)});
+      'hof/lib/logger': sinon.stub().returns(loggerStub)
+    });
   });
 
   afterEach(() => {
@@ -45,21 +56,21 @@ describe('generateReports', () => {
 
   it('should create a data directory if it does not exist', async () => {
     fsStub.existsSync.returns(false);
-    pathStub.join.returns('/mock/path/to/data');
 
     await generateReports();
 
-    expect(fsStub.existsSync.calledWith('/mock/path/to/data')).to.be.true;
-    expect(fsStub.mkdirSync.calledWith('/mock/path/to/data')).to.be.true;
+    expect(fsStub.existsSync.calledOnce).to.be.true;
+    expect(fsStub.mkdirSync.calledOnceWith(mockDataPath)).to.be.true;
   });
 
   it('should not create a data directory if it exists', async () => {
     fsStub.existsSync.returns(true);
-    pathStub.join.returns('/mock/path/to/data');
     createReportStub.rejects(new Error('Failed to create report'));
 
     await generateReports();
 
+    expect(fsStub.existsSync.calledOnce).to.be.true;
     expect(fsStub.mkdirSync.notCalled).to.be.true;
+    expect(loggerStub.info.calledWith('error')).to.be.true;
   });
 });

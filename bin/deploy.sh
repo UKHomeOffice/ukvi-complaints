37e2c239a65c36c7455eb4004f1ce098b34dca2e
Jon holder <jonathan.holder1@digital.homeoffice.gov.uk>
2026-07-31 17:15:53 +0100

    added pvc to service

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,49 +10,123 @@ export CONFIGMAP_VALUES=$HOF_CONFIG/configmap-values.yaml
 export NGINX_SETTINGS=$HOF_CONFIG/nginx-settings.yaml
 
 kd='kd --insecure-skip-tls-verify --timeout 10m --check-interval 10s'
+redis_storage_files='kube/redis/redis-persistent-volume-claim.yml'
+redis_runtime_files='kube/redis/redis-service.yml -f kube/redis/redis-network-policy.yml -f kube/redis/redis-deployment.yml'
+
+export REDIS_PERSISTENCE_ENABLED=${REDIS_PERSISTENCE_ENABLED:-}
+export REDIS_PERSISTENCE_EXISTING_CLAIM=${REDIS_PERSISTENCE_EXISTING_CLAIM:-}
+export REDIS_PERSISTENCE_ACCESS_MODES=${REDIS_PERSISTENCE_ACCESS_MODES:-ReadWriteOnce}
+export REDIS_PERSISTENCE_STORAGE_CLASS=${REDIS_PERSISTENCE_STORAGE_CLASS:-gp2-encrypted}
+export REDIS_PERSISTENCE_SIZE=${REDIS_PERSISTENCE_SIZE:-}
+export REDIS_PERSISTENCE_ANNOTATIONS_FILE=${REDIS_PERSISTENCE_ANNOTATIONS_FILE:-}
+export REDIS_DELETE_PVC_ON_TEARDOWN=${REDIS_DELETE_PVC_ON_TEARDOWN:-false}
+
+sanitize_branch_name() {
+  echo "$1" | tr '[:upper:]' '[:lower:]' | sed -E 's/[^a-z0-9]+/-/g; s/^-+//; s/-+$//'
+}
+
+set_redis_persistence() {
+  REDIS_PERSISTENCE_ENABLED=$(echo "$REDIS_PERSISTENCE_ENABLED" | tr '[:upper:]' '[:lower:]')
+  export REDIS_PERSISTENCE_ENABLED
+
+  case "${KUBE_NAMESPACE}" in
+    "${PROD_ENV}")
+      export REDIS_PERSISTENCE_ENABLED=${REDIS_PERSISTENCE_ENABLED:-true}
+      export REDIS_PERSISTENCE_SIZE=${REDIS_PERSISTENCE_SIZE:-10Gi}
+      ;;
+    "${STG_ENV}")
+      export REDIS_PERSISTENCE_ENABLED=${REDIS_PERSISTENCE_ENABLED:-true}
+      export REDIS_PERSISTENCE_SIZE=${REDIS_PERSISTENCE_SIZE:-1Gi}
+      ;;
+    *)
+      export REDIS_PERSISTENCE_ENABLED=false
+      export REDIS_PERSISTENCE_SIZE=${REDIS_PERSISTENCE_SIZE:-1Gi}
+      ;;
+  esac
+}
+
+should_create_redis_pvc() {
+  [[ (${KUBE_NAMESPACE} == ${STG_ENV} || ${KUBE_NAMESPACE} == ${PROD_ENV}) && ${REDIS_PERSISTENCE_ENABLED} == "true" && -z "${REDIS_PERSISTENCE_EXISTING_CLAIM}" ]]
+}
+
+deploy_redis() {
+  if should_create_redis_pvc; then
+    $kd -f $redis_storage_files
+  fi
+
+  $kd -f $redis_runtime_files
+}
+
+delete_redis() {
+  if [[ ${REDIS_DELETE_PVC_ON_TEARDOWN} == "true" ]] && should_create_redis_pvc; then
+    $kd --delete -f $redis_storage_files
+  fi
+
+  $kd --delete -f $redis_runtime_files
+}
+
+deploy_namespace() {
+  if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
+    $kd -f kube/configmaps -f kube/certs
+    deploy_redis
+    $kd -f kube/hof-rds-api -f kube/file-vault -f kube/app -f kube/cron
+  elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
+    $kd -f kube/configmaps/configmap.yml
+    deploy_redis
+    $kd -f kube/hof-rds-api -f kube/file-vault -f kube/app -f kube/cron
+  elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
+    $kd -f kube/configmaps/configmap.yml -f kube/hof-rds-api -f kube/file-vault -f kube/app/service.yml
+    deploy_redis
+    $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml -f kube/cron
+    $kd -f kube/app/deployment.yml
+  elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
+    $kd -f kube/configmaps/configmap.yml -f kube/hof-rds-api -f kube/file-vault -f kube/app/service.yml -f kube/cron
+    deploy_redis
+    $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
+    $kd -f kube/app/deployment.yml
+  fi
+}
+
+print_environment_urls() {
+  if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
+    echo "Branch internal - ukvic-$DRONE_SOURCE_BRANCH.branch.internal.sas-notprod.homeoffice.gov.uk"
+    echo "ukvic-$DRONE_SOURCE_BRANCH.branch.sas-notprod.homeoffice.gov.uk"
+  elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
+    echo "UAT internal 1 - ukvic.uat.internal.sas-notprod.homeoffice.gov.uk"
+    echo "UAT internal 2 - $APP_NAME.uat.internal.sas-notprod.homeoffice.gov.uk"
+    echo "UAT external - $APP_NAME.uat.sas-notprod.homeoffice.gov.uk"
+  elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
+    echo "STG internal 1 - stg.internal.ukvic.sas.homeoffice.gov.uk"
+    echo "STG internal 2 - stg.internal.$APP_NAME.sas.homeoffice.gov.uk"
+    echo "STG external - www.preprod.notprod.$APP_NAME.homeoffice.gov.uk"
+  elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
+    echo "PROD external - $PRODUCTION_URL"
+  fi
+}
 
 if [[ $1 == 'tear_down' ]]; then
   export KUBE_NAMESPACE=$BRANCH_ENV
-  export DRONE_SOURCE_BRANCH=$(cat /root/.dockersock/branch_name.txt)
+  export DRONE_SOURCE_BRANCH=$(sanitize_branch_name "$(cat /root/.dockersock/branch_name.txt)")
+  set_redis_persistence
 
   $kd --delete -f kube/configmaps/configmap.yml
-  $kd --delete -f kube/redis -f kube/app -f kube/file-vault -f kube/hof-rds-api
-  echo "Torn Down UAT Branch - ukvic-$DRONE_SOURCE_BRANCH.internal.$BRANCH_ENV.homeoffice.gov.uk"
+  delete_redis
+  $kd --delete -f kube/app -f kube/file-vault -f kube/hof-rds-api
+  echo "Torn Down Branch - ukvic-$DRONE_SOURCE_BRANCH.internal.$BRANCH_ENV.homeoffice.gov.uk"
   exit 0
 fi
 
-export KUBE_NAMESPACE=$1
-export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower:]' | tr '/' '-')
-
-if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
-  $kd -f kube/configmaps -f kube/certs
-  $kd -f kube/redis -f kube/hof-rds-api -f kube/file-vault -f kube/app  -f kube/cron
-elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml 
-  $kd -f kube/redis -f kube/hof-rds-api -f kube/file-vault -f kube/app  -f kube/cron
-elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml -f kube/hof-rds-api -f kube/file-vault -f kube/app/service.yml
-  $kd -f kube/redis -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml  -f kube/cron
-  $kd -f kube/app/deployment.yml
-elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml -f kube/redis -f kube/hof-rds-api -f kube/file-vault -f kube/app/service.yml  -f kube/cron
-  $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
-  $kd -f kube/app/deployment.yml
+if [[ -z "$1" ]]; then
+  echo "Usage: bin/deploy.sh <namespace|tear_down>"
+  exit 1
 fi
+
+export KUBE_NAMESPACE=$1
+export DRONE_SOURCE_BRANCH=$(sanitize_branch_name "$DRONE_SOURCE_BRANCH")
+set_redis_persistence
+
+deploy_namespace
 
 sleep $READY_FOR_TEST_DELAY
 
-if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
-  echo "Branch internal - ukvic-$DRONE_SOURCE_BRANCH.branch.internal.sas-notprod.homeoffice.gov.uk"
-  echo "ukvic-$DRONE_SOURCE_BRANCH.branch.sas-notprod.homeoffice.gov.uk"
-elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
-  echo "UAT internal 1 - ukvic.uat.internal.sas-notprod.homeoffice.gov.uk"
-  echo "UAT internal 2 - $APP_NAME.uat.internal.sas-notprod.homeoffice.gov.uk"
-  echo "UAT external - $APP_NAME.uat.sas-notprod.homeoffice.gov.uk"
-elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
-  echo "STG internal 1 - stg.internal.ukvic.sas.homeoffice.gov.uk"
-  echo "STG internal 2 - stg.internal.$APP_NAME.sas.homeoffice.gov.uk"
-  echo "STG external - www.preprod.notprod.$APP_NAME.homeoffice.gov.uk"
-elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
-  echo "PROD external - $PRODUCTION_URL"
-fi
+print_environment_urls
